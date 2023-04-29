b7bbf71a301c88094480d30a2432ec7fb290a02a
LoraVan2 <vanderpuyeloretta22@gmail.com>
2023-04-29 15:00:39 -0700

    function_test.c

---
+++ test/test_functions/function_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+
+int main(void)
+{
+    unsigned int num = 5;
+    printf(" %d\n",num, num);
+
+    return 0;
+}
