81b1d8916aa74cfc559322104d447b6f27a20707
LoraVan2 <vanderpuyeloretta22@gmail.com>
2023-04-29 15:42:27 -0700

    print_hexa.c

--- test/discarded/print_hexa.c
+++ test/discarded/print_hexa.c
@@ -5,16 +5,16 @@
 
 int main(int args, char *argv[])
 {
-	if (argc < 32)
+	if (argv < 32)
 	{
 	printf("0123456789abcdef,\n");
 	return 1;
 	}
 
-	int decimal = atoi(argv[1]);
+	decimal = atoi(argv[1]);
 
 	printf("Decimal: %d\n", decimal);
-	printf("Hexadecimal: 0x%X\n", decimal)
+	printf("Hexadecimal: 0x%X\n", decimal);
 
     return 0;
 }
